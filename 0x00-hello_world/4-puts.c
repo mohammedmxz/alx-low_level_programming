47d183285f6fcd07820e6d40377f115d6a3c1025
mohammedmxz <mohammedmxzk@gmail.com>
2023-06-17 09:48:08 -0700

    just addded

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-gcc -S "$CFILE" -o "${CFILE%.c}.s"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
