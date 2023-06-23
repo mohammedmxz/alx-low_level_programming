9abf24061b35be8743dd290c003140f2803f6133
mohammedmxz <mohammedmxzk@gmail.com>
2023-06-22 19:44:03 -0700

    just add

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,21 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
-/**
-*main - header file for protoypes
-*void protoypes (void)
-*int protoypes (int)
-*/
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
-int _putchar(char);
-int _isupper(int);
-int _isdigit(int);
-int mul(int, int);
-
-#endif /* MAIN_H *//
+#ifndef MAIN
+#define MAIN
+extern int _putchar(char c);
+extern int _isupper(int c);
+extern int _isdigit(int c);
+extern int mul(int a, int b);
+extern void print_numbers(void);
+extern void print_most_numbers(void);
+extern void more_numbers(void);
+extern void print_line(int n);
+extern void print_diagonal(int n);
+extern void print_square(int size);
+extern void print_triangle(int size);
+extern void print_number(int n);
+#endif
