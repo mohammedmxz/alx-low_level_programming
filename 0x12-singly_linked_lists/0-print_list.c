93727cde11a56703b9b183b5208c110d66847083
mohammedmxz <mohammedmxzk@gmail.com>
2023-07-28 18:55:50 -0700

    just added

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,27 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_list - Prints all the elements of a list_t list
+ * print_list - Prints all the elements of a list_t list.
+ * @h: Pointer to the head of the linked list
  *
- * @h: Pointer to the head of the first node
- *
- * Return: The number of nodes in the linked list
+ * Return: The number of nodes in the list.
  */
 size_t print_list(const list_t *h)
 {
-    size_t node_count = 0;
+    size_t node_count = 0; // Initialize node_count to 0
 
-    while (h != NULL)
+    while (h != NULL) // Loop through the linked list until the end
     {
+        // If str is NULL, print [0] (nil)
         if (h->str == NULL)
-            printf("[%u] %s\n", 0, "(nil)");
+            printf("[0] (nil)\n");
         else
             printf("[%u] %s\n", h->len, h->str);
 
-        h = h->next;
-        node_count++;
+        h = h->next; // Move to the next node
+        node_count++; // Increment the node_count
     }
 
-    return (node_count);
+    return (node_count); // Return the total number of nodes
 }
 
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,60 +1,25 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct list_s - Singly linked list node
  * @str: Pointer to a dynamically allocated string
  * @len: Length of the string
  * @next: Pointer to the next node in the list
  *
- * Description: A singly linked list node structure for Holberton project.
+ * Description: Singly linked list node structure for Holberton project.
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
-/**
- * print_list - Prints all the elements of a linked list.
- * @h: Pointer to the head of the linked list
- *
- * Return: The number of nodes in the list.
- */
 size_t print_list(const list_t *h);
-
-/**
- * list_len - Calculates the number of elements in a linked list.
- * @h: Pointer to the head of the linked list
- *
- * Return: The number of nodes in the list.
- */
-size_t list_len(const list_t *h);
-
-/**
- * add_node - Adds a new node at the beginning of a linked list.
- * @head: Double pointer to the head of the linked list
- * @str: Pointer to the string to be stored in the new node
- *
- * Return: Pointer to the new node.
- */
-list_t *add_node(list_t **head, const char *str);
-
-/**
- * add_node_end - Adds a new node at the end of a linked list.
- * @head: Double pointer to the head of the linked list
- * @str: Pointer to the string to be stored in the new node
- *
- * Return: Pointer to the new node.
- */
-list_t *add_node_end(list_t **head, const char *str);
-
-/**
- * free_list - Frees the memory allocated for a linked list.
- * @head: Pointer to the head of the linked list
- */
-void free_list(list_t *head);
 
 #endif /* LISTS_H */
 
