d6636778fb3c518820878fe11223325ec0b6a069
mohammedmxz <mohammedmxzk@gmail.com>
2023-07-28 19:11:27 -0700

    just added

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include "lists.h"
-
 /**
  * list_len - Returns the number of elements in a linked list.
  * @h: Pointer to the head of the list_t linked list.
@@ -9,14 +8,12 @@
  */
 size_t list_len(const list_t *h)
 {
-    size_t elements_count = 0; /* Initialize the count of elements to 0 */
+	size_t n = 0;
 
-    while (h != NULL) /* Loop through the linked list until the end */
-    {
-        elements_count++; /* Increment the count for each element */
-        h = h->next; /* Move to the next node */
-    }
-
-    return (elements_count); /* Return the total number of elements */
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
 }
-
