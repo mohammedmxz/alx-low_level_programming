0e3f522f677db70f2a3b190ff473b9089499930a
mohammedmxz <mohammedmxzk@gmail.com>
2023-07-28 19:04:11 -0700

    just added

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,21 +8,20 @@
  *
  * Return: The number of nodes in the linked list
  */
+
 size_t print_list(const list_t *h)
 {
-    size_t node_count = 0;
+	size_t s = 0;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[%u] %s\n", 0, "(nil)");
-        else
-            printf("[%u] %s\n", h->len, h->str);
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
 
-        h = h->next;
-        node_count++;
-    }
-
-    return (node_count);
+	return (s);
 }
-
