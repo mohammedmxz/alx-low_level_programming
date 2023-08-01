333f0005930c9bbb44f240aa80f80e4012719fa5
mohammedmxz <mohammedmxzk@gmail.com>
2023-08-01 13:40:08 -0700

    just added

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+
+struct listint_s - This is a singly linked list node structure
+@n: The integer data stored in the node
+@next: A pointer to the next node in the linked list
+Description: This structure is used for singly linked lists in the ALX project
+*/
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+#endif
