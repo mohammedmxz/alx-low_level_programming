529180354af9a5ef14f11d06ccbb5de6c5b6410c
mohammedmxz <mohammedmxzk@gmail.com>
2024-07-11 15:25:28 +0000

    just added

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,12 @@
-int _putchar(char c);
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int is_root(int n, int i);
 int is_prime_number(int n);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _putchar(char c);
+int is_divisible(int n, int i);
+int is_palindrome(char *s);
+int _strlen_recursion(char *s);
+int compare(char *str1, char* str2);
